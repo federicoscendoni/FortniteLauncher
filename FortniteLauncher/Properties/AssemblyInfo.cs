98c789dd40170c0d9c10023c568ec55770616d55
federicoscendoni <152164251+federicoscendoni@users.noreply.github.com>
2023-12-20 16:31:00 +0100

    update file

---
+++ FortniteLauncher/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("fLauncher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ElLondiuh")]
+[assembly: AssemblyProduct("fLauncher")]
+[assembly: AssemblyCopyright("Copyright © ElLondiuh")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("bafeef0a-f055-41ef-bd77-854074c1c4b4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
